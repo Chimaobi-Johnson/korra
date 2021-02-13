08cbede5d7ca10d7846427772598b22738812894
Chimaobi Wogu <woguchimaobi@gmail.com>
2021-02-13 18:06:03 +0100

    added functionality for drawer navigation

--- navigation/DrawerContent.js
+++ navigation/DrawerContent.js
@@ -1,35 +1,47 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
     StyleSheet,
     Text,
     TouchableOpacity,
     View,
-    FlatList
+    FlatList,
+    Image
   } from "react-native";
 import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { MainContext } from '../mainContext';
 
-  const menuData = [
+const menuData = [
     { icon: faUser, name: "Profile", screenName: "UserProfile", key: "1" },
     { icon: faHome, name: "Home", screenName: "Feed", key: "2" },
-  ];
+];
   
-const DrawerContent = props => (
-    <View style={styles.container}>
-        <FlatList
-            data={menuData}
-            renderItem={({ item }) => (
-                <DrawerItem
-                    navigation={props.navigation}
-                    screenName={item.screenName}
-                    icon={item.icon}
-                    name={item.name}
-                    key={item.key}
-                />
-        )}
-        />
-    </View>
-);
+const DrawerContent = props => {
+    const userData = useContext(MainContext);
+    if(!userData) {
+       return <h4>Loading...</h4>
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.profileContainer}>
+                <Image source={userData.profilePhoto ? userData.profilePhoto : require('../assets/images/avatar-1577909_640.png')} style={styles.profileImage} />
+                <Text style={styles.profileName}>{userData.lastName + ' ' + userData.firstName}</Text>
+            </View>
+            <FlatList
+                data={menuData}
+                renderItem={({ item }) => (
+                    <DrawerItem
+                        navigation={props.navigation}
+                        screenName={item.screenName}
+                        icon={item.icon}
+                        name={item.name}
+                        key={item.key}
+                    />
+            )}
+            />
+        </View>
+    )
+}
 
 const DrawerItem = ({ navigation, icon, name, screenName }) => (
     <TouchableOpacity
@@ -49,7 +61,8 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: "rgba(255,255,255,0.43)",
-      paddingTop: 70
+      paddingTop: 70,
+      paddingLeft: 20
     },
     menuItem: {
       flexDirection: "row"
@@ -71,8 +84,23 @@ const styles = StyleSheet.create({
       margin: 15
     },
     icon: {
-        color: '#fff5b1',
+        color: '#464646',
         marginTop: 3
+    },
+    profileContainer: {
+        borderBottomColor: '#eaeaea',
+        borderBottomWidth: 1
+    },
+    profileImage: {
+        width: 70,
+        height: 70,
+        borderRadius: 100
+    },
+    profileName: {
+        fontWeight: 'bold',
+        fontSize: 13,
+        marginTop: 20,
+        paddingBottom: 15
     }
 });
 
