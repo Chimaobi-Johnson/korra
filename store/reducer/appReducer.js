c3e406c42048b52a3176e86d67f0fa00f337c3df
Chimaobi Wogu <woguchimaobi@gmail.com>
2021-02-13 19:22:03 +0100

    added reducer for question and answer

--- store/reducer/appReducer.js
+++ store/reducer/appReducer.js
@@ -1,15 +1,149 @@
 import * as actionTypes from "../actions/actionTypes";
 
 const initialState = {
-    user: null
+    questions: {
+        data: null,
+        status: null,
+        loading: false
+    },
+    question: {
+        data: null,
+        status: null,
+        loading: false
+    },
+    answers: {
+        data: null,
+        status: null,
+        loading: false
+    },
+    answer: {
+        data: null,
+        status: null,
+        loading: false
+    }
 }
 
 export default (state = initialState, action) => {
     switch(action.type) {
-        case actionTypes.STORE_USER_DATA:
+        case actionTypes.FETCH_QUESTION:
             return {
                 ...state,
-                user: action.payload
+                question: {
+                    ...state.question,
+                    data: null,
+                    status: null,
+                    loading: true
+                }
+            }
+        case actionTypes.FETCH_QUESTION_SUCCESS:
+            return {
+                ...state,
+                question: {
+                    ...state.question,
+                    data: action.payload,
+                    status: action.payload.status,
+                    loading: false
+                }
+            }
+        case actionTypes.FETCH_QUESTION_FAIL: 
+            return {
+                ...state,
+                question: {
+                    ...state.question,
+                    data: null,
+                    status: action.payload.status,
+                    loading: false
+                }
+            }
+        case actionTypes.FETCH_QUESTIONS:
+            return {
+                ...state,
+                questions: {
+                    ...state.questions,
+                    data: null,
+                    status: null,
+                    loading: true
+                }
+            }
+        case actionTypes.FETCH_QUESTIONS_SUCCESS:
+            return {
+                ...state,
+                questions: {
+                    ...state.questions,
+                    data: action.payload,
+                    status: action.payload.status,
+                    loading: false
+                }
+            }
+        case actionTypes.FETCH_QUESTIONS_FAIL:
+            return {
+                ...state,
+                questions: {
+                    ...state.questions,
+                    data: null,
+                    status: action.payload.status,
+                    loading: false
+                }
+            }
+        case actionTypes.FETCH_ANSWER:
+            return {
+                ...state,
+                answer: {
+                    ...state.answer,
+                    data: null,
+                    status: null,
+                    loading: true
+                }
+            }
+        case actionTypes.FETCH_ANSWER_SUCCESS:
+            return {
+                ...state,
+                answer: {
+                    ...state.answer,
+                    data: action.payload,
+                    status: action.payload.status,
+                    loading: false
+                }
+            }
+        case actionTypes.FETCH_ANSWER_FAIL: 
+            return {
+                ...state,
+                answer: {
+                    ...state.answer,
+                    data: null,
+                    status: action.payload.status,
+                    loading: false
+                }
+            }
+        case actionTypes.FETCH_ANSWERS:
+            return {
+                ...state,
+                answers: {
+                    ...state.questions,
+                    data: null,
+                    status: null,
+                    loading: true
+                }
+            }
+        case actionTypes.FETCH_ANSWERS_SUCCESS:
+            return {
+                ...state,
+                questions: {
+                    ...state.questions,
+                    data: action.payload,
+                    status: action.payload.status,
+                    loading: false
+                }
+            }
+        case actionTypes.FETCH_ANSWERS_FAIL:
+            return {
+                ...state,
+                questions: {
+                    ...state.questions,
+                    data: null,
+                    status: action.payload.status,
+                    loading: false
+                }
             }
         default:
          return state
